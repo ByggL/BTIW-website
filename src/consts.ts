a4f8a88b346f3674099d8124f891e1f013977bb0
Samuel <samuel.leobon@gmail.com>
2023-11-23 15:07:35 +0100

    some basic changes and setups

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'The World Beyond The Ice Wall';
+export const SITE_DESCRIPTION = 'A worldbuilding project where every conspiracy theory is real';
