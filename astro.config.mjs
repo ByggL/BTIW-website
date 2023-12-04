61d2fb288693465aadf26fca9e797a574a99b170
Samuel <samuel.leobon@gmail.com>
2023-12-04 10:05:54 +0100

    fixed images, added Separator and placeholder text

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import { astroImageTools } from "astro-imagetools";
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
-	integrations: [mdx(), sitemap(), astroImageTools],
+	integrations: [mdx(), sitemap()],
 });
